eeac0a2d0a7c0dd21586c1546e0286dc88650ede
Jaxon <rogersj2@rose-hulman.edu>
2025-01-30 09:00:44 -0500

    Leaving reviews implemented as well as practical changes for debugging

--- src/eventplanner/Main.java
+++ src/eventplanner/Main.java
@@ -39,49 +39,24 @@ public class Main {
             // test register and login in command-line
             Scanner scanner = new Scanner(System.in);
             UserService userService = new UserService(dbService);
-
-            System.out.println("\n=== USER REGISTRATION ===");
-            System.out.print("Enter Email: ");
-            String email = scanner.nextLine();
-            System.out.print("Enter Phone Number: ");
-            String phoneNo = scanner.nextLine();
-            System.out.print("Enter First Name: ");
-            String firstName = scanner.nextLine();
-            System.out.print("Enter Middle Initial (or press Enter to skip): ");
-            String middleInit = scanner.nextLine();
-            System.out.print("Enter Last Name: ");
-            String lastName = scanner.nextLine();
-            System.out.print("Enter Date of Birth (YYYY-MM-DD): ");
-            String dob = scanner.nextLine();
-            System.out.print("Enter Credit Card Number (or press Enter to skip): ");
-            String ccNum = scanner.nextLine();
-            System.out.print("Enter CC Expiry Date (YYYY-MM-DD, or press Enter to skip): ");
-            String ccExpDate = scanner.nextLine();
-            System.out.print("Enter CVV (or press Enter to skip): ");
-            String cvv = scanner.nextLine();
-            System.out.print("Enter Password: ");
-            String password = scanner.nextLine();
-
-            boolean registered = userService.registerUser(email, phoneNo, firstName, middleInit, lastName, dob, ccNum, ccExpDate, cvv, password);
-
-            if (registered) {
-                System.out.println("Registration successful!");
-            } else {
-                System.out.println("Registration failed.");
+            
+            while(true) {
+            	System.out.println("Login or Register? l to login, r to register, c to close,\nand lr to leave a review");
+                String choice = scanner.nextLine().trim();
+                System.out.println(choice);
+	            if(choice.equals("l")) {
+	            	login(scanner, userService);
+	            } else if(choice.equals("r")) {
+	            	register(scanner,userService); 
+	            } else if(choice.equals("lr")) {
+	            	leaveReview(scanner,userService);
+	            } else if(choice.equals("c")) {
+	            	break;
+	            }
             }
+            
 
-            System.out.println("\n=== USER LOGIN ===");
-            System.out.print("Enter Email: ");
-            String loginEmail = scanner.nextLine();
-            System.out.print("Enter Password: ");
-            String loginPassword = scanner.nextLine();
-
-            boolean loginSuccess = userService.loginUser(loginEmail, loginPassword);
-            if (loginSuccess) {
-                System.out.println("Login successful!");
-            } else {
-                System.out.println("Login failed. Invalid email or password.");
-            }
+           
 
             // test AvailableEventsService in command line
             // TODO: test AvailableEventsService on webpage (API required)
@@ -106,7 +81,58 @@ public class Main {
         }
     }
 
-    public static Properties loadProperties() {
+    private static void leaveReview(Scanner scanner, UserService userService) {
+    	System.out.println("\n=== LEAVE REVIEW ===");
+    	System.out.print("Enter PersonID: ");
+    	int personID = scanner.nextInt();
+    	scanner.nextLine();  
+
+    	System.out.print("Enter VenueID: ");
+    	Integer venueID = scanner.nextInt();
+    	scanner.nextLine();  
+
+    	System.out.print("Enter EventID: ");
+    	Integer eventID = scanner.nextInt();
+    	scanner.nextLine();  
+
+    	System.out.print("Enter Title (Can be empty): ");
+    	String title = scanner.nextLine();
+
+    	System.out.print("Enter Rating (1-5): ");
+    	int rating = scanner.nextInt();
+    	scanner.nextLine();  
+
+    	System.out.print("Enter Description (Can be empty): ");
+    	String desc = scanner.nextLine();
+
+    	System.out.print("Enter PostedOn (MM-DD-YYYY HH:MM:SS.DDD): ");
+    	String postedOn = scanner.nextLine();
+
+        boolean registered = userService.leaveReview(personID, venueID, eventID, title, rating, desc, postedOn);
+        if (registered) {
+            System.out.println("Review successful!");
+        } else {
+            System.out.println("Review failed.");
+        }
+		
+	}
+
+	private static void login(Scanner scanner, UserService userService) {
+    	 System.out.println("\n=== USER LOGIN ===");
+         System.out.print("Enter Email: ");
+         String loginEmail = scanner.nextLine();
+         System.out.print("Enter Password: ");
+         String loginPassword = scanner.nextLine();
+
+         boolean loginSuccess = userService.loginUser(loginEmail, loginPassword);
+         if (loginSuccess) {
+             System.out.println("Login successful!");
+         } else {
+             System.out.println("Login failed. Invalid email or password.");
+         }
+	}
+
+	public static Properties loadProperties() {
         String configPath = System.getProperty("user.dir") + "/EventPlannerApp.properties";
         StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
         encryptor.setPassword(es.getEncryptionPassword());
@@ -134,5 +160,37 @@ public class Main {
             }
         }
         return props;
+    }
+    
+    private static void register(Scanner scanner, UserService userService) {
+    	System.out.println("\n=== USER REGISTRATION ===");
+        System.out.print("Enter Email: ");
+        String email = scanner.nextLine();
+        System.out.print("Enter Phone Number: ");
+        String phoneNo = scanner.nextLine();
+        System.out.print("Enter First Name: ");
+        String firstName = scanner.nextLine();
+        System.out.print("Enter Middle Initial (or press Enter to skip): ");
+        String middleInit = scanner.nextLine();
+        System.out.print("Enter Last Name: ");
+        String lastName = scanner.nextLine();
+        System.out.print("Enter Date of Birth (YYYY-MM-DD): ");
+        String dob = scanner.nextLine();
+        System.out.print("Enter Credit Card Number (or press Enter to skip): ");
+        String ccNum = scanner.nextLine();
+        System.out.print("Enter CC Expiry Date (YYYY-MM-DD, or press Enter to skip): ");
+        String ccExpDate = scanner.nextLine();
+        System.out.print("Enter CVV (or press Enter to skip): ");
+        String cvv = scanner.nextLine();
+        System.out.print("Enter Password: ");
+        String password = scanner.nextLine();
+
+        boolean registered = userService.registerUser(email, phoneNo, firstName, middleInit, lastName, dob, ccNum, ccExpDate, cvv, password);
+
+        if (registered) {
+            System.out.println("Registration successful!");
+        } else {
+            System.out.println("Registration failed.");
+        }
     }
 }
--- src/eventplanner/services/UserService.java
+++ src/eventplanner/services/UserService.java
@@ -7,10 +7,12 @@ import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Types;
 import java.util.Base64;
 import java.util.Random;
 
@@ -126,5 +128,67 @@ public class UserService { ;
         }
         return getStringFromBytes(hash);
     }
+	
+	/**
+	 * Registers a new review left by the user for a specified venue
+	 *
+	 * @return true if registration succeeds, false otherwise.
+	 */
+	public boolean leaveReview(int PersonID, Integer VenueID, Integer EventID, String Title, int Rating,
+	                            String Desc, String PostedOn) {
+	    Connection conn = dbService.getConnection();
+	    if (conn == null) {
+	        JOptionPane.showMessageDialog(null, "Database connection failed.");
+	        return false;
+	    }
+	
+	    try {
+	        String query = "{? = call dbo.AddReview(?, ?, ?, ?, ?, ?, ?)}";
+	        CallableStatement stmt = conn.prepareCall(query);
+	        
+	        stmt.registerOutParameter(1, Types.INTEGER);
+	        stmt.setInt(2, PersonID);
+	        
+	        if (VenueID == -1) 
+	            stmt.setNull(3, Types.INTEGER);
+	         else
+	            stmt.setInt(3, VenueID);
+	        
+	        if (EventID == -1)
+	            stmt.setNull(4, Types.INTEGER);
+	        else 
+	            stmt.setInt(4, EventID);
+	        
+	        stmt.setString(5, Title.isEmpty() ? null : Title);   // Nullable field
+	        stmt.setInt(6, Rating);
+	        stmt.setString(7, Desc.isEmpty() ? null : Desc); // Nullable field
+	        stmt.setString(8, PostedOn);
+	        
+	        stmt.execute();
+	        
+	     // Get the return code
+ 			int returnCode = stmt.getInt(1);
 
+ 			// Print return code
+ 			if (returnCode == 0) {
+ 	            System.out.println("Success!");
+ 	        } else if (returnCode == 1) {
+ 	        	JOptionPane.showMessageDialog(null, "ERROR: Required Fields cannot be empty");
+ 	        } else if (returnCode == 2) {
+ 	        	JOptionPane.showMessageDialog(null, "ERROR: Specified Person, Venue, and/or Event do not exist.");
+ 	        } else if (returnCode == 3) {
+ 	        	JOptionPane.showMessageDialog(null, "ERROR: Same user cannot leave more than 1 review per Event or Venue.");
+ 	        } else if (returnCode == 4) {
+ 	        	JOptionPane.showMessageDialog(null, "ERROR: Date is not a valid date.");
+ 	        } else if (returnCode == 5) {
+ 	        	JOptionPane.showMessageDialog(null, "ERROR: Rating must be between 1 and 5.");
+ 	        } else
+ 	        	JOptionPane.showMessageDialog(null, "ERROR: Unknown error has occured.");
+	        return returnCode == 0;
+	    } catch (SQLException e) {
+	        System.err.println("Error leaving review: " + e.getMessage());
+	        e.printStackTrace();
+	        return false;
+	    }
+	}
 }
